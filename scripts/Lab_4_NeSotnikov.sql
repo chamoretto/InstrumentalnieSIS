96e438db3b81653114dc35080f050d447a86e878
George Chamor <chamor.work@gmail.com>
2021-05-20 10:18:29 +0300

    Пример функции

--- scripts/Lab_4_NeSotnikov.sql
+++ scripts/Lab_4_NeSotnikov.sql
@@ -9,3 +9,43 @@
 --Ежеквартальная планировка полётов
 --Данные о рейсах и полётах
 --Расписание полётов на квартал
+
+
+-- Темплейтик функции
+
+CREATE OR REPLACE FUNCTION process_tasks_journals(OUT void) RETURNS void
+    LANGUAGE plpgsql AS
+$$
+DECLARE
+    task tasks;
+BEGIN
+    IF TG_OP = 'INSERT' THEN
+        task = NEW;
+        IF NOT EXISTS(SELECT 1 FROM any_table aee WHERE aee.entity_id = task.id) THEN
+            IF (SELECT id FROM tasks WHERE task_id = NEW.id) IS NULL THEN
+                INSERT INTO tasks (id)
+                values ();
+            END IF;
+        END IF;
+        RETURN task;
+    ELSIF TG_OP = 'UPDATE' THEN
+        task = NEW;
+        NEW.updated_at = current_timestamp;
+        IF OLD.total_work_time = NEW.total_work_time THEN
+            IF OLD.total_cost = NEW.total_cost THEN
+                INSERT INTO tasks (id)
+                values (task.id);
+            END IF;
+        END IF;
+        RETURN task;
+    ELSIF TG_OP = 'DELETE' THEN
+        task = OLD;
+        INSERT INTO tasks (id)
+        values (task.id);
+        DELETE
+        FROM any_table as aee
+        WHERE aee.entity_id = task.id;
+        RETURN task;
+    END IF;
+END;
+$$
