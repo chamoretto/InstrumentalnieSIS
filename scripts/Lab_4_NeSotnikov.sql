9c9277ea0643dd5cb3d97594cdbf4e5dd2ee7dbe
Tea-set <sotnikovid@gmail.com>
2021-05-21 10:30:47 +0300

    yes

--- scripts/Lab_4_NeSotnikov.sql
+++ scripts/Lab_4_NeSotnikov.sql
@@ -7,8 +7,8 @@ CREATE OR REPLACE FUNCTION flight_rescheduling(id_flight INT, new_time TIMESTAMP
 $$
 BEGIN
     UPDATE flight SET departure_datetime = new_time WHERE flight.flight_id = id_flight;
-    RETURN(SELECT customer_id FROM customer WHERE customer_id = (SELECT customer_id FROM ticket WHERE flight_id = (SELECT flight_id FROM flight WHERE flight.flight_id = id_flight))
-                                     OR (SELECT customer_id FROM reservation WHERE flight_id = (SELECT flight_id FROM flight WHERE flight.flight_id = id_flight)));
+    RETURN(SELECT customer_id FROM customer WHERE customer_id = (SELECT customer_id FROM ticket WHERE flight_id = id_flight)
+                                     OR (SELECT customer_id FROM reservation WHERE flight_id = id_flight));
 END;
 $$;
 
