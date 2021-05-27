05fc95450d343718f18d30c13c2e55e7e6a78e1d
George Chamor <chamor.work@gmail.com>
2021-05-27 10:16:50 +0300

    Поправил запросы, добавил коды возврата

--- scripts/Lab_4_Sotnikov.sql
+++ scripts/Lab_4_Sotnikov.sql
@@ -17,7 +17,7 @@ $$;
 -- ОПИСАНИЕ: Выпуск билета после оплаты брони
 -- ОБЛАСТЬ: Информационные системы аэропорта
 -- Электронное/физическое воплощение билета после оплаты брони
-CREATE OR REPLACE FUNCTION create_ticket_by_paid_reservation(paid_reservation json, place_ int) RETURNS void
+CREATE OR REPLACE FUNCTION create_ticket_by_paid_reservation(paid_reservation json) RETURNS int
     LANGUAGE plpgsql AS
 $$
 BEGIN
@@ -26,31 +26,37 @@ BEGIN
     VALUES ((paid_reservation ->> 'customer_id')::int,
             (paid_reservation ->> 'flight_id')::int,
             (paid_reservation ->> 'reservation_id')::int,
-            place_,
+            (paid_reservation ->> 'place')::int,
             CURRENT_TIMESTAMP,
             true);
+    RETURN 0;
 
 EXCEPTION
     -- если ошибка - печатаем в консоль. транзакция откатится автоматически
     WHEN OTHERS THEN
-        RAISE NOTICE 'ОШИБКА!';
+        RETURN 1;
 END;
 $$;
 
 SELECT create_ticket_by_paid_reservation(
-               (SELECT row_to_json(r.*) FROM reservation r WHERE r.reservation_id = 3 LIMIT 1),
-               22222
-           );
+   (SELECT row_to_json(r.*) FROM reservation r WHERE r.reservation_id = 3 LIMIT 1)
+);
 
 ---------------------------------------------------------------------------------------------------------------
 
 
 DROP FUNCTION IF EXISTS reservation_canceling(id_reservation INT);
-CREATE OR REPLACE FUNCTION reservation_canceling(id_reservation INT) RETURNS VOID
+CREATE OR REPLACE FUNCTION reservation_canceling(id_reservation INT) RETURNS int
     LANGUAGE plpgsql AS
 $$
 BEGIN
     UPDATE reservation SET is_cancelled = true WHERE reservation.reservation_id = id_reservation;
+    RETURN 0;
+
+EXCEPTION
+    -- если ошибка - печатаем в консоль. транзакция откатится автоматически
+    WHEN OTHERS THEN
+        RETURN 1;
 END;
 $$;
 
